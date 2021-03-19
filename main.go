04aa72ad52d2969949b3c12bda6662b623a30870
ayumin <kosyu79@gmail.com>
2021-03-19 15:40:34 +0900

    mysql接続部分作成

--- main.go
+++ main.go
@@ -1,19 +1,61 @@
 package main
 
-import "github.com/labstack/echo"
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo"
+)
+
+var db *sqlx.DB
 
 func init() {
-	// MYSQL接続
+	if err := godotenv.Load(); err != nil {
+		log.Println("info: ", err)
+	}
 
+	// MYSQL接続
+	mysqlUser := os.Getenv("MYSQL_USER")
+	mysqlPass := os.Getenv("MYSQL_PASSWORD")
+	mysqlAddr := os.Getenv("MYSQL_ADDRESS")
+	mysqlPort := os.Getenv("MYSQL_PORT")
+	mysqlDBName := os.Getenv("MYSQL_DATABASE")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", mysqlUser, mysqlPass, mysqlAddr, mysqlPort, mysqlDBName)
+	log.Println("info: DSN ->", dsn)
+	for {
+		db, err = sqlx.Connect("mysql", dsn)
+		if err != nil {
+			log.Println("SQL Connect Error\n", err)
+			time.Sleep(time.Second * 10)
+			continue
+		}
+		break
+	}
+
+	schema := `
+	CREATE TABLE IF NOT EXISTS yuki-data (
+		device_id CHAR(40) NOT NULL,
+		points    BIGINT   NOT NULL,
+		date      DATE     NOT NULL
+	);
+	`
+	db.MustExec(schema)
 }
 
 func main() {
 	e := echo.New()
 	// hard - api
-	//	e.POST("/post", handlePOST)
+	e.POST("/post", handlePOST)
+
 	//	e.GET("/data/each", handleEachData)
 	//	e.GET("/data/total", handleTotalData)
 
+	// テスト用
 	e.GET("/test/each", handleTestEach)
 	e.GET("/test/total", handleTestTotal)
 
